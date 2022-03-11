d50ee2220fef92c2fe06bfe950106445fb7061c3
aschrad1 <aschrad1@swarthmore.edu>
2022-03-10 23:40:05 -0500

    login functionality

--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -10,7 +10,7 @@ import UIKit
 import BDBOAuth1Manager
 
 class TwitterAPICaller: BDBOAuth1SessionManager {    
-    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "5lUJuO5AUpPUCez4ewYDFrtgh", consumerSecret: "s5ynGqXzstUZwFPxVyMDkYh197qvHOcVM3kwv1o2TKhS1avCdS")
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "3ahOttBGUdLANqRgnItr6YpXW", consumerSecret: "HiwzZgpLuUTk9IwVGHK0Rldv7sGcbmUUcD4GvkMI9r55z8fZA4")
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
     
